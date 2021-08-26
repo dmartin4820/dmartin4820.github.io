d35b31b145101027e5fb4a142cb2c00057bd0836
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-08-25 22:19:39 -0700

    update projects to fetch from server and change to grid

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-
-const githubLight = './images/GitHub-Mark-Light-120px-plus.png'; 
-const githubDark = './images/GitHub-Mark-120px-plus.png';
+import {githubDark, githubLight} from '../assets/images';
 
 const imgStyle = {
 	width: 600,
@@ -9,21 +7,24 @@ const imgStyle = {
 const linkStyle ={
 	width: 40,
 }
-const projectCard = 'flex m-2 w-1/2 p-5 transition duration-100 ease-in-out hover:bg-green-300 rounded-lg hover:shadow-2xl hover:text-white transform hover:-translate-y-1';
-const heading = 'text-2xl mb-2';
+const projectCard = 'flex flex-col m-2 p-5 transition duration-100 ease-in-out hover:bg-green-300 rounded-lg hover:shadow-2xl hover:text-white transform hover:-translate-y-1';
+const heading = 'text-2xl mb-2 inline';
 const img = 'rounded-lg';
-const descContainer = 'p-10 w-1/2';
+const descContainer = 'p-2';
 const skillList = 'flex flex-wrap p-5';
 
 function Project(props) {
 	const [githubSrc, setGithubSrc] = useState(githubDark)
+	const [visible, setVisible] = useState('hidden');
 
 	function handleMouseEnter(e) {
 		setGithubSrc(githubLight);
+		setVisible('')
 	}
 
 	function handleMouseLeave(e) {
 		setGithubSrc(githubDark);
+		setVisible('hidden')
 	}
 
 	return (
@@ -32,14 +33,20 @@ function Project(props) {
 				<img className={img} style={imgStyle} src={props.image_link} alt='This is a placeholder'/>
 			</div>
 			<div className={descContainer}>
-				<a target="_blank" href={props.deployed_link} rel="noreferrer">
-					<h3 className={heading}>{props.title}</h3>
-				</a>
+
+				{props.deployed_link !== '' 
+					? <a target="_blank" href={props.deployed_link} rel="noreferrer">
+							<h3 className={heading}>{props.title}</h3>
+							<span className={`${visible} p-2 font-bold text-green-900`}>&larr; Visit Site</span>
+						</a>
+					: <h3 className={heading}>{props.title} (No deployed link)</h3>
+				}
+
 				<p>{props.description}</p>
 				<div className={skillList}>
 					{props.skills.map((skill, i) => {
 						return (
-							<p>
+							<p key={i}>
 								{skill.name}
 								{i !== props.skills.length - 1 
 									? <span className='mx-1'>&bull;</span>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Project from '../Project';
 
-const projectContainer = 'flex flex-col items-center mt-10 mb-48';
-
+// const projectContainer = 'flex flex-col items-center mt-10 mb-48';
+const projectContainer = 'grid grid-cols-3 p-20'
+const url = 'https://dmartin.herokuapp.com/projects';
+// const url = 'http://localhost:3001/projects'; 
 
 function Projects(props) {
 	const [projects, setProjects] = useState([]);
@@ -13,17 +15,18 @@ function Projects(props) {
 	}, [])
 
 	async function getProjects() {
-		const results = await fetch('https://dmartin.herokuapp.com/projects');
+		const results = await fetch(url);
 		const data = await results.json();
 		setProjects(data)
 	}
 	
 	return (
 		<div className={projectContainer}>
-			{projects.map(project => {
+			{projects.map((project, i) => {
 				return (
 					<Project
 						{...project}
+						key={i}
 					/>
 				)
 			})}
